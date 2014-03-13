da3e44ae03b2fd055a4badebaa995a603e26acb7
Josh Gillham <usajoshgillham@gmail.com>
2014-03-12 20:33:50 -0700

    Finished updating comments for HW7.

--- HW7/HW7.s
+++ HW7/HW7.s
@@ -30,31 +30,39 @@ DecStr      %       12      ; Declares DecStr as to be 12 bytes.
     ADR R4, TwoComp                 ; Get the address of TwoComp.
 LOOP
     SWI 4                           ; Call system interupt to input number.
+;
+; For the following we expect the input to within '0' .. '9'
+; or within 'A' .. 'F'. Otherwise, the input is out of range.
+;
 ; For the numbers 0 .. 9, whose ASCII hex values are 0x31 and 0x39.
 ; Check to see if the input is a number character.
 ; If '0' < input < '9'
-    CMP R0, #&30                    ; Compare to 0x30 or '0'.
-    BLT BAD_INPUT                   ; Bad since the ASCII code is below 0x30.
+    CMP R0, #&30                    ; Compare input to 0x30 or '0'.
+    BLT BAD_INPUT                   ; Input is out of range.
     CMP R0, #&39                    ; Compare input to 0x39 or '9'
     BGT CHECK_LETTERS               ; Input is not a number character.
+    ; Why 0x30? For ASCII '0', we want the number zero.
+    ; Stated as a formula: 0x30 - x = 0x00.
     SUB R2, R0, #&30                ; Translate the ASCII into a number.
     B END_IF                        ; Go to the end of the 'if' statement.
-; For the letters A .. F
+; For the letters A .. F, whose ASCII hex values are 0x41 and 0x46.
 ; If &41 < input < &46
 CHECK_LETTERS
-    CMP R0, #&41
-    BLT BAD_INPUT
-    CMP R0, #&46
-    BGT BAD_INPUT
-    SUB R2, R0, #&37
-    
-
+    CMP R0, #&41                    ; Compare input to 0x41 or 'A'.
+    BLT BAD_INPUT                   ; Input is out of range.
+    CMP R0, #&46                    ; Compare input to 0x46 or 'F'.
+    BGT BAD_INPUT                   ; Input is out of range.
+    ; Why 0x37? For ASCII 0x41 or 'A', we want the number to be 0xA.
+    ; Stated as a formula: 0x41 - x = 0x0A.
+    SUB R2, R0, #&37                ; Translate the ASCII into a number.
 END_IF
     
-    STRB R2, [R4], #1               ; Store R4 into r2
-    CMP R1, #8                      ; Compare R1 to 8.
-    BLT LOOP
- 
+    STRB R2, [R4], #1               ; Use the R4 pointer to store the result.
+    CMP R1, #8                      ; This loop will continue only 8 rounds.
+    BLT LOOP                        ; Repeat while on or under 8 rounds.
+
+;-------------------------------------------------------------------------------
+; The following code has not been tested and is under construction.
     ADR R4, TwoComp
     LDR R2, [R4]
     TST R2, #&80000000              ; Test if r2 is negative.
@@ -81,6 +89,8 @@ PRINT_NUMBERS
 PRINT_LETTERS
     ADD R0, R0, #&37                ; Change the value into a ASCII code.
     SWI SWI_WriteC
+;-------------------------------------------------------------------------------
+
 BAD_INPUT    
     SWI SWI_Exit                    ; Exit the program
 
